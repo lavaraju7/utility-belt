445653de7bd672bd28595c1fade9f1c25ff7d8c3
koruprolu lava raju <lavaraju1170183@gmail.com>
2025-04-19 20:16:47 +0530

    added logo

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="RCP11pq7PelD-YDRFsV6SwuELJZLsfEvtDE5mwkF6zk"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
