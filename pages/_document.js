0472f6be37744fab8f1853ef8870be950c28ec3d
koruprolu lava raju <lavaraju1170183@gmail.com>
2025-04-19 23:30:08 +0530

    issue fixing

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="RCP11pq7PelD-YDRFsV6SwuELJZLsfEvtDE5mwkF6zk"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="RCP11pq7PelD-YDRFsV6SwuELJZLsfEvtDE5mwkF6zk"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
